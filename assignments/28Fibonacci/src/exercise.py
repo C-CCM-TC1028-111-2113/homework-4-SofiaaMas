ba0a8ba39f99f56d24a8ed6daa4cd2a5d11e5109
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-09-04 23:34:23 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    a=0
+b=1
+c=0
+while True:
+    n=(int(input('Inserte un número >=0:')))
+    if n>=0:
+        break
+print('0',end=' ')
+for i in range(1,n):
+    c=a+b
+    print(f"{c}",end=' ')
+    a=b
+    b=c
+print('')
     pass
 
 if __name__=='__main__':
