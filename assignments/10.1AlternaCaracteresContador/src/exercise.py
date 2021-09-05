aece28f142ea64ec31b3dcfb848e8e003bf557a4
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-09-04 22:15:39 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input('Inserte la cantidad de símbolos a repetir:'))
+for numero in range(n):
+    if numero%2 == 0:
+        print('#')
+
+    else:
+        print('%')
     pass
 
 if __name__=='__main__':   
